23a9f7859fb87a114fa7186b9a07e80659c2f063
IHaveTheSourceCode <58383582+IHaveTheSourceCode@users.noreply.github.com>
2022-06-08 21:39:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,3 @@ function Book(title, author, pages, read) {
     console.log(title, "by " + author, pages + " pages,", read);
   };
 }
-
-function addBookToLibrary(book) {
-  myLibrary += book;
-}
-
-const Hobbit = new Book("Hobbit", "Tolkien", "356", "yes");
-
-function Show(array) {
-  for (let i = 0; i <= 0; i++) {}
-}
